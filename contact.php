e0af7de94314f1cd927b30b167e190d8b12485d2
Beatriz Montero <bmonterom01@informatica.iesvalledeljerteplasencia.es>
2024-10-08 21:01:05 +0200

    proyecto

--- contact.php
+++ contact.php
@@ -1,3 +1,28 @@
 <?php
-    require "views/contact.views.php"
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $errores = [];
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $email = $_POST['email'];
+    $asunto = $_POST['asunto'];
+    $mensaje = $_POST['mensaje'];
+
+    if (empty($nombre)) {
+        $errores[] = "El campo First Name no puede estar vacío";
+    }
+
+    if (empty($email)) {
+        $errores[] = "El campo Email está vacío";
+    }
+    
+    if (empty($asunto)) {
+        $errores[] = "El campo Subject está vacío";
+    } 
+
+}
+
+require "views/contact.views.php"
+
 ?>
